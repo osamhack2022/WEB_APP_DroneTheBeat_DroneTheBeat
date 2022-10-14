359100e1c6d356293493ac56973ce4314ec73921
yjjo8136 <66682023+yjjo8136@users.noreply.github.com>
2022-10-14 06:35:23 +0000

    DetailPage Firebase connect

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DetailPage extends StatelessWidget {
   final String docID;
@@ -12,35 +13,93 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(),
       body: ListView(
         children: [
-          Text('비행 허가 요청 부대'),
-          Text('드론 기종'),
-          Text('기간'),
-          Text('비행목적'),
-          Row(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseFirestore.instance
-                      .collection('flight_info')
-                      .doc(docID)
-                      .update({'accepted': true});
-                },
-                child: Text('승인'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseFirestore.instance
-                      .collection('flight_info')
-                      .doc(docID)
-                      .update({'accepted': false});
-                },
-                child: Text('반려'),
-              ),
-            ],
-          )
+          StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('flight_info')
+                .doc(docID)
+                .snapshots(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              return Column(
+                children: [
+                  Text(snapshot.data['army']),
+                  Text(snapshot.data['model']),
+                  Text(snapshot.data['duration']),
+                  Text(snapshot.data['purpose']),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 300,
+                    child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(snapshot.data['location'].latitude,
+                            snapshot.data['location'].longitude),
+                        zoom: 16,
+                      ),
+                      markers: {
+                        Marker(
+                          markerId: MarkerId(
+                              'marker_id_${DateTime.now().millisecondsSinceEpoch}'),
+                          position: LatLng(snapshot.data['location'].latitude,
+                              snapshot.data['location'].longitude),
+                          icon: BitmapDescriptor.defaultMarker,
+                          infoWindow: const InfoWindow(
+                            title: '주소',
+                            snippet: '비행 반경',
+                          ),
+                        ),
+                      },
+                      circles: {
+                        Circle(
+                          circleId: CircleId(
+                              'circle_id_${DateTime.now().millisecondsSinceEpoch}'),
+                          center: LatLng(snapshot.data['location'].latitude,
+                              snapshot.data['location'].longitude),
+                          fillColor: Colors.blue.shade100.withOpacity(0.5),
+                          strokeColor: Colors.blue.shade100.withOpacity(0.1),
+                          radius: double.parse(snapshot.data['radius']),
+                        ),
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+          SizedBox(height: 10),
+          _buildAcceptButton(context),
         ],
       ),
+    );
+  }
+
+  Widget _buildAcceptButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('flight_info')
+                .doc(docID)
+                .update({'accepted': true});
+            Navigator.pop(context);
+          },
+          child: Text('승인'),
+        ),
+        SizedBox(width: 10),
+        ElevatedButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('flight_info')
+                .doc(docID)
+                .update({'accepted': false});
+            Navigator.pop(context);
+          },
+          child: Text('반려'),
+        ),
+      ],
     );
   }
 }
